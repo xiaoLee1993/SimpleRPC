0a4e5d88f59fac9502e913a16bc57dceae01be39
xiaoLee <274389206@qq.com>
2020-04-17 09:48:58 +0800

    Simple RPC

--- src/com/xiaolee/client/RPCClient.java
+++ src/com/xiaolee/client/RPCClient.java
@@ -1,15 +1,10 @@
 package com.xiaolee.client;
 
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class RPCClient {
 
@@ -26,41 +21,32 @@ public class RPCClient {
 
     public <T> T remote(Class<T> interfaceClass) {
         return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[] { interfaceClass },
-                new InvocationHandler() {
+                (Object proxy, Method method, Object[] args)->{
 
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        // TODO Auto-generated method stub
-                        Socket socket = new Socket(host, port);
-                        System.out.println(host);
+                    Socket socket = new Socket(host, port);
+                    System.out.println(host);
+                    try(ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
+
+                        output.writeUTF(interfaceClass.getName());
+                        output.writeUTF(method.getName());
+                        output.writeObject(method.getParameterTypes());
+                        output.writeObject(args);
+
+                        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
                         try {
-                            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-
-                            try {
-                                output.writeUTF(interfaceClass.getName());
-                                output.writeUTF(method.getName());
-                                output.writeObject(method.getParameterTypes());
-                                output.writeObject(args);
-
-                                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-
-                                try {
-                                    Object result = input.readObject();
-                                    if (result instanceof Throwable) {
-                                        throw (Throwable) result;
-                                    }
-                                    return result;
-                                } finally {
-                                    input.close();
-                                }
-
-                            } finally {
-                                output.close();
+                            Object result = input.readObject();
+                            if (result instanceof Throwable) {
+                                throw (Throwable) result;
                             }
-
+                            return result;
                         } finally {
-                            socket.close();
+                            input.close();
                         }
+
+
+                    } finally {
+                        socket.close();
                     }
                 });
 
